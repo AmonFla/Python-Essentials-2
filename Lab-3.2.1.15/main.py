1b55a8d7f155b5c8ddd5f995081701596c3cf29f
Flavio Garrido <fbonnin@teceng-gaming.com.ar>
2022-03-23 23:38:43 -0300

    Lab-3.2.1.15

---
+++ Lab-3.2.1.15/main.py
@@ -0,0 +1,29 @@
+class QueueError(LookupError):  # Choose base class for the new exception.
+    #  Write code here
+    pass
+
+
+class Queue:
+    def __init__(self):
+        self.__queue = []
+
+    def put(self, elem):
+        self.__queue.insert(0,elem)
+
+    def get(self):
+        if len(self.__queue) == 0:
+            raise QueueError()
+        val = self.__queue[-1]
+        del self.__queue[-1]
+        return val
+
+
+que = Queue()
+que.put(1)
+que.put("dog")
+que.put(False)
+try:
+    for i in range(4):
+        print(que.get())
+except:
+    print("Queue error")
