1b0555e71cb0c6f20cf6a3a66f33f7c71d63119a
Flavio Garrido <fbonnin@teceng-gaming.com.ar>
2022-03-24 12:13:06 -0300

    Lab-3.4.1.14

---
+++ Lab-3.4.1.14/main.py
@@ -0,0 +1,27 @@
+import math
+
+
+class Point:
+    def __init__(self, x=0.0, y=0.0):
+        self.__x = x
+        self.__y = y
+
+    def getx(self):
+        return self.__x
+
+    def gety(self):
+        return self.__y
+
+    def distance_from_xy(self, x, y):
+        
+        x = self.__x - x
+        y = self.__y - y
+        return math.sqrt(x**2+y**2)
+
+    def distance_from_point(self, point):
+        return self.distance_from_xy(point.getx(),point.gety())
+
+point1 = Point(0, 0)
+point2 = Point(1, 1)
+print(point1.distance_from_point(point2))
+print(point2.distance_from_xy(2, 0))
